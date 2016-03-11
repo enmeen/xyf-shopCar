72651149dd08710e6a6277a1eae558b3b95e4f85
enmeen <enmeen007@gmail.com>
2016-03-11 18:03:03 +0800

    w3c认证  编写nav部分代码
    研究浮动与负边距的关系。做test

--- js/xyf-shopCar.js
+++ js/xyf-shopCar.js
@@ -1,6 +1,7 @@
 window.onload = function() {
     changeRem();
-    keepFont();
+    switchNav();
+
     /*自适应更换rem*/
     function changeRem() {
         var ww = $(document.body).width();
@@ -23,4 +24,52 @@ window.onload = function() {
             })
         }
     }
+    /*nav切换事件*/
+    function switchNav() {
+        var navLi = $('.nav-ul').eq(0).children('li');
+        //获取当前li的marginleft值
+        var ccc = listMargintop();
+     console.log(ccc);
+        navLi.each(function() { //绑定tap事件
+            $(this).on('tap', function(e) {
+                e.stopPropagation(); //防止冒泡
+                navLi.each(function() { //重置on类
+                    $(this).removeClass('on');
+                });
+                var listcls = e.target.className;
+                var nub = listcls.substr(-1, 1);
+                var ccc = listMargintop();
+                console.log(ccc);
+                navLi.each(function(index) {
+                   /* console.log(index, nub);*/
+                    if (index < nub) { $(this).animate({ 'margin-left': ccc[index] - ccc[nub] + 'px' });
+                        }
+                    if (index == nub) {
+                        $(this).animate({ 'margin-left': -ccc[nub ] + 'px' });
+                     
+                    }
+
+
+                })
+              
+
+                $(this).addClass('on');
+
+                /*var liLeft = $(this).offset().left; //移动选中项目至最左端
+                 $(this).animate({ 'margin-left': -liLeft + 'px' });*/
+
+            })
+        })
+
+    }
+    /*提取nav li 距离左端的距离*/
+    function listMargintop() {
+        var listLeft = [];
+        var navLi = $('.nav-ul').eq(0).children('li');
+        navLi.each(function(index) {
+            listLeft.push($(this).offset().left);
+        })
+        return listLeft;
+    }
+
 }
