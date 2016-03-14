c83b09581bcf4203dace319e6d78004e36851805
enmeen <enmeen007@gmail.com>
2016-03-14 11:22:21 +0800

    添加了nav交互
    将jq换为zepto，针对移动端

--- js/xyf-shopCar.js
+++ js/xyf-shopCar.js
@@ -1,75 +1,52 @@
 window.onload = function() {
-    changeRem();
-    switchNav();
-
+    "use strict";
     /*自适应更换rem*/
-    function changeRem() {
+    function changeRem(htmFont, screenWidth) {
         var ww = $(document.body).width();
         var htm = $('html');
-        console.log(ww);
-        var math = ww * 32 / 640;
-        var percentage = ww / 640;
-        console.log(percentage);
+        var math = ww * htmFont / screenWidth;
+        var percentage = ww / screenWidth;
         htm.css('font-size', math + 'px');
-        console.log(htm.css('font-size'));
-    };
-    /*确保价格字体大于等于16px*/
-    function keepFont() {
-        var productPrice = $('.product-price');
-        var ppfs = productPrice.eq(0).css('font-size');
-        console.log(ppfs);
-        if (parseInt(ppfs) < 16) {
-            productPrice.each(function() {
-                $(this).css('font-size', '16px')
-            })
-        }
     }
-    /*nav切换事件*/
-    function switchNav() {
-        var navLi = $('.nav-ul').eq(0).children('li');
-        //获取当前li的marginleft值
-        var ccc = listMargintop();
-     console.log(ccc);
+    /*nav点击切换事件*/
+    function switchNav(parele, chiele, actcls) {
+        var n;
+        var navUl = $(parele).eq(0);
+        var navLi = navUl.children(chiele);
+        var navli_left = navLi.eq(1).offset().left - navUl.offset().left;
+        //获取当前li的marginleft值;
         navLi.each(function() { //绑定tap事件
             $(this).on('tap', function(e) {
                 e.stopPropagation(); //防止冒泡
-                navLi.each(function() { //重置on类
-                    $(this).removeClass('on');
-                });
-                var listcls = e.target.className;
-                var nub = listcls.substr(-1, 1);
-                var ccc = listMargintop();
-                console.log(ccc);
-                navLi.each(function(index) {
-                   /* console.log(index, nub);*/
-                    if (index < nub) { $(this).animate({ 'margin-left': ccc[index] - ccc[nub] + 'px' });
-                        }
-                    if (index == nub) {
-                        $(this).animate({ 'margin-left': -ccc[nub ] + 'px' });
-                     
-                    }
+                $(this).siblings().removeClass(actcls); //重置其余兄弟元素上的class
+                $(this).addClass(actcls);
+                console.log($(this).index());
+                n = $(this).index();
+                navUl.animate({ 'margin-left': -n * navli_left + 'px' });
 
-
-                })
-              
-
-                $(this).addClass('on');
-
-                /*var liLeft = $(this).offset().left; //移动选中项目至最左端
-                 $(this).animate({ 'margin-left': -liLeft + 'px' });*/
-
-            })
-        })
+                //fn
+            });
+        });
 
     }
-    /*提取nav li 距离左端的距离*/
-    function listMargintop() {
-        var listLeft = [];
-        var navLi = $('.nav-ul').eq(0).children('li');
-        navLi.each(function(index) {
-            listLeft.push($(this).offset().left);
-        })
-        return listLeft;
-    }
 
-}
+    /*右滑返回顶部*/
+    function turnRight() {
+        var nav = $('.nav').eq(0);
+        var activeClass = $('.nav-ul .on').eq(0);
+        var firstLi = $('.nav-ul li').eq(0);
+        var navUl = $('.nav-ul').eq(0);
+        console.log(firstLi);
+        nav.on('swipeRight', function() {
+            firstLi.siblings().removeClass('on');
+            firstLi.addClass('on');
+            navUl.animate({'margin-left':0});
+
+        })
+    }
+   
+    changeRem(32, 640);
+    switchNav('.nav-ul', 'li', 'on');
+    turnRight();
+
+};
