3ca00cf16b26a8e0fed68d860eae337b3badf454
enmeen <enmeen007@gmail.com>
2016-03-14 13:08:56 +0800

    添加了reszie事件

--- js/xyf-shopCar.js
+++ js/xyf-shopCar.js
@@ -20,11 +20,8 @@ window.onload = function() {
                 e.stopPropagation(); //防止冒泡
                 $(this).siblings().removeClass(actcls); //重置其余兄弟元素上的class
                 $(this).addClass(actcls);
-                console.log($(this).index());
                 n = $(this).index();
                 navUl.animate({ 'margin-left': -n * navli_left + 'px' });
-
-                //fn
             });
         });
 
@@ -36,17 +33,20 @@ window.onload = function() {
         var activeClass = $('.nav-ul .on').eq(0);
         var firstLi = $('.nav-ul li').eq(0);
         var navUl = $('.nav-ul').eq(0);
-        console.log(firstLi);
         nav.on('swipeRight', function() {
             firstLi.siblings().removeClass('on');
             firstLi.addClass('on');
-            navUl.animate({'margin-left':0});
+            navUl.animate({ 'margin-left': 0 });
 
         })
     }
-   
+    /*刷新时的初始化*/
     changeRem(32, 640);
     switchNav('.nav-ul', 'li', 'on');
     turnRight();
-
+    $(window).resize(function() { //屏幕尺寸发生变化时，执行。
+        changeRem(32, 640);
+        switchNav('.nav-ul', 'li', 'on');
+        turnRight();
+    })
 };
