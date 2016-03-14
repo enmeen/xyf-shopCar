40e645eded7651d761c8d50e1f765c89ed8b0c0c
enmeen <enmeen007@gmail.com>
2016-03-14 16:45:39 +0800

    同事帮助下的修改
    1.物品窗口换用了a标签
    2.优化了js自适应rem事件
    3.nav滑动自适应到达最后一个就停止，防止多余空白出现
    4.nav菜单滑到最后一个以后，半透明光影效果隐藏。

--- js/xyf-shopCar.js
+++ js/xyf-shopCar.js
@@ -1,34 +1,36 @@
-window.onload = function() {
+window.onload = function () {
     "use strict";
-    /*自适应更换rem*/
-    function changeRem(htmFont, screenWidth) {
-        var ww = $(document.body).width();
-        var htm = $('html');
-        var math = ww * htmFont / screenWidth;
-        var percentage = ww / screenWidth;
-        htm.css('font-size', math + 'px');
-    }
     /*nav点击切换事件*/
     function switchNav(parele, chiele, actcls) {
         var n;
         var navUl = $(parele).eq(0);
         var navLi = navUl.children(chiele);
         var navli_left = navLi.eq(1).offset().left - navUl.offset().left;
+        var shadow = $('.nav .shadow').eq(0);
+        console.log(shadow);
         //获取当前li的marginleft值;
-        navLi.each(function() { //绑定tap事件
+        navLi.each(function () { //绑定tap事件
             $(this).on('tap', function(e) {
                 e.stopPropagation(); //防止冒泡
                 $(this).siblings().removeClass(actcls); //重置其余兄弟元素上的class
                 $(this).addClass(actcls);
                 n = $(this).index();
-                navUl.animate({ 'margin-left': -n * navli_left + 'px' });
+                var offleft = n * navli_left;
+                var maxleft = navli_left*navLi.length - $('.nav').eq(0).width();
+                if (offleft > maxleft) {//判断是否到尾部
+                    navUl.animate({ 'transform': 'translateX(' + (-maxleft) + 'px)' });
+                    shadow.hide();
+                } else {
+                    navUl.animate({ 'transform': 'translateX(' + (-n * navli_left) + 'px)' });
+                    shadow.show();
+                }
+
             });
         });
 
     }
-
     /*右滑返回顶部*/
-    function turnRight() {
+    function turnRight () {
         var nav = $('.nav').eq(0);
         var activeClass = $('.nav-ul .on').eq(0);
         var firstLi = $('.nav-ul li').eq(0);
@@ -36,17 +38,17 @@ window.onload = function() {
         nav.on('swipeRight', function() {
             firstLi.siblings().removeClass('on');
             firstLi.addClass('on');
-            navUl.animate({ 'margin-left': 0 });
+            navUl.animate({ 'transform': 'translateX(0)' });
 
         })
     }
     /*刷新时的初始化*/
-    changeRem(32, 640);
+
     switchNav('.nav-ul', 'li', 'on');
     turnRight();
-    $(window).resize(function() { //屏幕尺寸发生变化时，执行。
-        changeRem(32, 640);
+    $(window).resize(function () { //屏幕尺寸发生变化时，执行。
+
         switchNav('.nav-ul', 'li', 'on');
         turnRight();
-    })
+    });
 };
