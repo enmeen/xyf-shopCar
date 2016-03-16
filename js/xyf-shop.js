329e66909ca702dab6dbefc93595ac540c0491b2
enmeen <enmeen007@gmail.com>
2016-03-16 17:00:19 +0800

    版本上线后的更新
    1.严格模式
    2.将原来的循环添加事件，改为了事件代理的方式。优化性能。
    3.删除了末尾的resize事件。因为实现自适应的代码一些在html文件开头

--- js/xyf-shop.js
+++ js/xyf-shop.js
@@ -1,5 +1,6 @@
 window.onload = function() {
     /*nav点击、滑动切换事件*/
+    'use strict';
     function switchNav() {
         var n = undefined;
         var navUl = $('.nav-ul').eq(0);
@@ -9,7 +10,36 @@ window.onload = function() {
         var maxleft = navli_left * navLi.length - $('.nav').eq(0).width(); //导航栏目超出nav宽度的距离。即像右滑动的最大距离
         var nav = $('.nav').eq(0);
         //获取当前li的marginleft值;
-        navLi.each(function() { //绑定tap事件
+        nav.live('tap', function(e) {//1.采用事件委托
+            var eTar = e.target;
+            var $eTar = $(eTar);//转化为jq对象
+            var n = $eTar.index();
+            var offleft = n * navli_left;
+            $eTar.siblings().removeClass('on');
+            $eTar.addClass('on');
+            var tagTar = eTar.tagName.toLocaleLowerCase();
+            if (tagTar == 'li') {
+                if (maxleft > 0 && offleft > maxleft) { //移动距离 大于 最大距离 
+                    navUl.css('margin-left', -maxleft + 'px');
+                    shadow.hide();
+                }
+                if (maxleft > 0 && offleft < maxleft) {
+                    if (!n) {
+                        navUl.css('margin-left', 0);
+                    } else {
+                        navUl.css('margin-left', -(n - 1) * navli_left + 'px');
+                        shadow.show();
+                    }
+
+                }
+                if (maxleft < 0) { //导航栏项目较小，不必触发滑动
+                    shadow.hide();
+                }
+            }
+        })
+
+/*
+        navLi.each(function() { //2.原始方达。绑定tap事件
             $(this).on('tap', function(e) {
                 e.stopPropagation(); //防止冒泡
                 $(this).siblings().removeClass('on'); //重置其余兄弟元素上的class
@@ -34,11 +64,8 @@ window.onload = function() {
                 }
 
             });
-        });
+        });*/
     }
     /*刷新时的初始化*/
     switchNav();
-    $(window).resize(function() { //屏幕尺寸发生变化时，执行。
-        switchNav();
-    });
 };
